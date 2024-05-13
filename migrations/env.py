04a8f39e704d5019ffb9badcd3aad0136dfe1ed2
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-14 00:34:15 +0700

    Removed Song

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from sqlmodel import SQLModel
 
-from models import Song
+from models import *
 from settings import settings
 
 # this is the Alembic Config object, which provides
