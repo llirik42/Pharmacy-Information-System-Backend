479f3572d043cdd3213e4556674046618bc65655
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-14 17:22:38 +0700

    Renamed table for routes

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class Base(DeclarativeBase):
 
 
 class User(Base):
-    __tablename__ = "administration_routes"
+    __tablename__ = "ADMINISTRATION_ROUTES"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     description: Mapped[str] = mapped_column(String(256), unique=True)
