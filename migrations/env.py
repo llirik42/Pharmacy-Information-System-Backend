6cb2e7fe3d6199546b76e84f31184e376a9ec702
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-14 17:11:54 +0700

    Добавил миграции

--- migrations/env.py
+++ migrations/env.py
@@ -1,12 +1,13 @@
 import asyncio
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from models import *
+from alembic import context
+
+from models import Base
 from settings import settings
 
 # this is the Alembic Config object, which provides
--- migrations/versions/0e1b9df7c58c_initial.py
+++ migrations/versions/0e1b9df7c58c_initial.py
@@ -1,8 +1,8 @@
-"""Initial revision
+"""initial
 
-Revision ID: 9d66be83e094
-Revises: 6624b288eb27
-Create Date: 2024-05-14 17:00:35.660824
+Revision ID: 0e1b9df7c58c
+Revises: 
+Create Date: 2024-05-14 17:11:20.462650
 
 """
 from typing import Sequence, Union
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '9d66be83e094'
-down_revision: Union[str, None] = '6624b288eb27'
+revision: str = '0e1b9df7c58c'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
