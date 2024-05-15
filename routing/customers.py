de843f1f40790108d2c3e45efb76d66f7275e35c
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-15 16:46:41 +0700

    Реализовал end-point для 12-го запроса

--- routing/customers.py
+++ routing/customers.py
@@ -1,13 +1,18 @@
 from datetime import date
+from http import HTTPStatus
 from typing import Optional
 
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import select, text
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import session
 
 from db import get_session
 from models import CustomerOrm
 from schemas import Customer, FrequentCustomer
+
+from fastapi import status
+from fastapi.exceptions import HTTPException
 
 router = APIRouter(prefix="/customers")
 
@@ -33,6 +38,81 @@ async def get_ordered_something_customers(
 
 @router.get("/frequent")
 async def get_frequent_customers(
-    drug_id: Optional[int] = None, drug_type_id: Optional[int] = None
+    drug_id: Optional[int] = None,
+    drug_type_id: Optional[int] = None,
+    session: AsyncSession = Depends(get_session)
 ) -> list[FrequentCustomer]:
-    return []
+    query = text(_get_frequent_customers_query_string(drug_id=drug_id, drug_type_id=drug_type_id))
+    result = await session.execute(query)
+
+    customers: list[FrequentCustomer] = []
+
+    for row in result:
+        q = await session.get(CustomerOrm, ident=row[0])
+        c = Customer.model_validate(q)
+
+        customers.append(FrequentCustomer(
+            customer=c,
+            orders_count=row[1],
+        ))
+
+    return customers
+
+
+def _get_frequent_customers_query_string(drug_id: Optional[int] = None,
+                                         drug_type_id: Optional[int] = None) -> str:
+    if drug_id is not None:
+        return f"""
+            select
+                id as customer_id,
+                orders_count
+            from (
+                select
+                    customers.id,
+                    count(*) as orders_count,
+                    dense_rank() over (order by count(*) desc) as dr
+                from orders
+                    join prescriptions_content using (prescription_id)
+                    join customers on orders.customer_id = customers.id
+                where drug_id = {drug_id}
+                group by customer_id
+                ) all_orders_count_data
+            where dr = 1
+        """
+
+    if drug_type_id is not None:
+        return f"""
+            select
+                id as customer_id,
+                orders_count
+            from (
+                select
+                    customers.id,
+                    count(*) as orders_count,
+                    dense_rank() over (order by count(*) desc) as dr
+                from orders
+                    join prescriptions_content using (prescription_id)
+                    join drugs on prescriptions_content.drug_id = drugs.id
+                    join customers on orders.customer_id = customers.id
+                where type_id = {drug_type_id}
+                group by customer_id
+                ) all_orders_count_data
+            where dr = 1
+        """
+
+    return """
+            select
+                id as customer_id,
+                orders_count
+            from (
+                select
+                    customers.id,
+                    count(*) as orders_count,
+                    dense_rank() over (order by count(*) desc) as dr
+                from orders
+                    join prescriptions_content using (prescription_id)
+                    join customers on orders.customer_id = customers.id
+                group by customer_id
+                ) all_orders_count_data
+            where dr = 1
+        """
--- schemas/frequent_customer.py
+++ schemas/frequent_customer.py
@@ -1,7 +1,9 @@
 from pydantic import Field
 
+from .base.base import Base
 from .base.customer import Customer
 
 
-class FrequentCustomer(Customer):
+class FrequentCustomer(Base):
+    customer: Customer
     orders_count: int = Field(ge=0)
