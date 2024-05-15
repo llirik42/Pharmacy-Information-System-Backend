0b31f36a83de1102cf2cba7fdda45e2c76e3fc9a
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-15 17:18:13 +0700

    Реализовал end-point для 5-го запроса

--- routing/customers.py
+++ routing/customers.py
@@ -46,9 +46,23 @@ async def get_waiting_supplies_customers(drug_type_id: Optional[int] = None, ses
 
 @router.get("/ordered-something")
 async def get_ordered_something_customers(
-    period_start: date, period_end: date, drug_id: Optional[int] = None, drug_type_id: Optional[int] = None
+    period_start: date, period_end: date, drug_id: Optional[int] = None, drug_type_id: Optional[int] = None, session: AsyncSession = Depends(get_session)
 ) -> list[Customer]:
-    return []
+    query = text(_get_ordered_something_customers_query_string(
+        period_start=period_start,
+        period_end=period_end,
+        drug_id=drug_id,
+        drug_type_id=drug_type_id
+    ))
+    result = await session.execute(query)
+
+    customers: list[Customer] = []
+
+    for row in result:
+        q = await session.get(CustomerOrm, ident=row[0])
+        customers.append(Customer.model_validate(q))
+
+    return customers
 
 
 @router.get("/frequent")
@@ -72,6 +86,46 @@ async def get_frequent_customers(
         ))
 
     return customers
+
+
+def _get_ordered_something_customers_query_string(period_start: date, period_end: date, drug_id: Optional[int] = None, drug_type_id: Optional[int] = None) -> str:
+    s1 = f"'{str(period_start).replace('-', '/')}'"
+    s2 = f"'{str(period_end).replace('-', '/')}'"
+
+    if drug_id is not None:
+        return f"""
+            select distinct
+                customer_id
+            from prescriptions_content
+                join orders using (prescription_id)
+            where
+                (registration_datetime between {s1} and {s2})
+                and (prescriptions_content.drug_id = {drug_id})
+                and customer_id is not null
+        """
+
+    if drug_type_id is not None:
+        return f"""
+            select distinct
+                customer_id
+            from prescriptions_content
+                join drugs on prescriptions_content.drug_id = drugs.id
+                join orders using (prescription_id)
+            where
+                (registration_datetime between {s1} and {s2})
+                and (drugs.type_id = {drug_type_id})
+                and customer_id is not null
+        """
+
+    return f"""
+        select distinct
+            customer_id
+        from prescriptions_content
+            join orders using (prescription_id)
+        where
+            (registration_datetime between {s1} and {s2})
+            and customer_id is not null
+    """
 
 
 def _get_frequent_customers_query_string(drug_id: Optional[int] = None,
