24a19ef066f0ddb024d93e6697b4ba015de0d1cd
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-15 15:22:59 +0700

    Реализовал end-point для 7

--- routing/drugs.py
+++ routing/drugs.py
@@ -62,8 +62,55 @@ async def get_critical_amount_drugs(session: AsyncSession = Depends(get_session)
 
 
 @router.get("/minimal-amount")
-async def get_minimal_amount_drugs(drug_type_id: Optional[int] = None) -> list[StoredDrug]:
-    return []
+async def get_minimal_amount_drugs(drug_type_id: Optional[int] = None, session: AsyncSession = Depends(get_session)) -> list[StoredDrug]:
+    tmp = "" if drug_type_id is None else f"where type_id = {drug_type_id}"
+
+    query_s = f"""
+        with
+            drugs_storage_amount as (
+                select
+                    drugs.id as drug_id,
+                    coalesce(sum(available_amount), 0) as drug_amount,
+                    critical_amount
+                from drugs
+                    left join storage_items on drugs.id = storage_items.drug_id
+                {tmp}
+                group by
+                    drugs.id,
+                    critical_amount
+            ),
+        
+            ranked_drugs as (
+                select
+                  drug_id,
+                  dense_rank() over (order by drug_amount) as dr,
+                  drug_amount
+              from drugs_storage_amount
+              group by drug_id
+            )
+        
+        select
+            drug_id,
+            drug_amount
+        from ranked_drugs
+        where dr = 1
+    """
+
+    query = text(query_s)
+
+    result = await session.execute(query)
+
+    drugs: list[StoredDrug] = []
+
+    for i in result:
+        res = await session.get(DrugOrm, ident=i[0])
+        drug = Drug.model_validate(res)
+        drugs.append(StoredDrug(
+            drug=drug,
+            stored_count=i[1]
+        ))
+
+    return drugs
 
 
 @router.get("/used")
--- schemas/stored_drug.py
+++ schemas/stored_drug.py
@@ -1,7 +1,9 @@
 from pydantic import Field
 
+from .base.base import Base
 from .base.drug import Drug
 
 
-class StoredDrug(Drug):
+class StoredDrug(Base):
+    drug: Drug
     stored_count: int = Field(ge=0)
