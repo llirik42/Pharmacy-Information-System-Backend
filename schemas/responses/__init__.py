9af280b83695bdb506ba6cad713f35193575f3fd
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-19 13:33:40 +0700

    Added description for OrderResponse and little refactoring

--- schemas/responses/__init__.py
+++ schemas/responses/__init__.py
@@ -1,10 +1,15 @@
 __all__ = [
-    'OrderStatus',
-    'OrderResponse',
-    'create_order_not_found_response',
+    "OrderStatus",
+    "OrderResponse",
+    "create_order_not_found_response",
     "create_order_internal_error_response",
-    "create_order_success_response"
+    "create_order_success_response",
 ]
 
 from .order_status import OrderStatus
-from .order_response import OrderResponse, create_order_not_found_response, create_order_internal_error_response, create_order_success_response
+from .order_response import (
+    OrderResponse,
+    create_order_not_found_response,
+    create_order_internal_error_response,
+    create_order_success_response,
+)
--- schemas/responses/order_response.py
+++ schemas/responses/order_response.py
@@ -1,10 +1,14 @@
+from pydantic import Field
+
 from .order_status import OrderStatus
 from ..base.base import Base
 
 
 class OrderResponse(Base):
     message: str
-    status: OrderStatus
+    status: OrderStatus = Field(
+        description="".join([f"{i} - {OrderStatus(i).name}, " for i in range(len(OrderStatus))])
+    )
     order_id: int = 0
 
 
@@ -13,16 +17,8 @@ def create_order_success_response(order_id: int) -> OrderResponse:
 
 
 def create_order_not_found_response(order_id: int) -> OrderResponse:
-    return OrderResponse(
-        message=f"Order not found",
-        status=OrderStatus.ORDER_NOT_FOUND,
-        order_id=order_id
-    )
+    return OrderResponse(message=f"Order not found", status=OrderStatus.ORDER_NOT_FOUND, order_id=order_id)
 
 
 def create_order_internal_error_response(order_id: int, message: str) -> OrderResponse:
-    return OrderResponse(
-        message=message,
-        status=OrderStatus.INTERNAL_ERROR,
-        order_id=order_id
-    )
+    return OrderResponse(message=message, status=OrderStatus.INTERNAL_ERROR, order_id=order_id)
